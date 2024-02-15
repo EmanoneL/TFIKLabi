38a0545d9d044d405373c4ac5c36f9b82c3bfbd2
Liticiya <54845415+Liticiya@users.noreply.github.com>
2024-02-15 14:43:15 +0700

    Add label

--- TFIKLabi/Form1.Designer.cs
+++ TFIKLabi/Form1.Designer.cs
@@ -29,56 +29,69 @@ namespace TFIKLabi
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new System.Windows.Forms.Button();
-            button2 = new System.Windows.Forms.Button();
-            button3 = new System.Windows.Forms.Button();
-            SuspendLayout();
-            // 
-            // button1
-            // 
-            button1.Location = new System.Drawing.Point(234, 214);
-            button1.Name = "button1";
-            button1.Size = new System.Drawing.Size(483, 168);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
             // 
             // button2
             // 
-            button2.Location = new System.Drawing.Point(234, 40);
-            button2.Name = "button2";
-            button2.Size = new System.Drawing.Size(483, 168);
-            button2.TabIndex = 1;
-            button2.Text = "button2";
-            button2.UseVisualStyleBackColor = true;
+            this.button2.Location = new System.Drawing.Point(234, 40);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(483, 168);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(234, 214);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(483, 168);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            button3.Location = new System.Drawing.Point(31, 85);
-            button3.Name = "button3";
-            button3.Size = new System.Drawing.Size(483, 168);
-            button3.TabIndex = 2;
-            button3.Text = "button3";
-            button3.UseVisualStyleBackColor = true;
+            this.button3.Location = new System.Drawing.Point(234, 270);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(483, 168);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
             // 
             // Form1
             // 
-            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
-            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            ClientSize = new System.Drawing.Size(800, 450);
-            Controls.Add(button3);
-            Controls.Add(button2);
-            Controls.Add(button1);
-            Name = "Form1";
-            Text = "Form1";
-            ResumeLayout(false);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
     }
 }
 
